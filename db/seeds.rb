253d608270e9e382cbdf4baea35baf3389699b2e
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-12-14 23:19:21 -0200

    added seed.rb (initial validation)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+o1 = Owner.create(name: 'teste1', email: 'teste1')
+o2 = Owner.create(name: 'teste2', email: 'teste2')
+o3 = Owner.create(name: 'teste3', email: 'teste3')
+Certificate.create(cn: 'teste1', owner: o1)
+Certificate.create(cn: 'teste2', owner: o1)
+Certificate.create(cn: 'teste3', owner: o1)
+Certificate.create(cn: 'teste4', owner: o2)
